6c0c0ecb4a3ce3535db1ac82e94af1724466d80a
Tyler Nigon <nigo0024@ln1002.mangi.msi.umn.edu>
2020-11-16 21:06:15 -0600

    cleaning up jobs directory

---
+++ jobs/tune_train/sip_2_tune_train_89-92.sh
@@ -0,0 +1,39 @@
+#!/bin/bash -l
+#SBATCH --job-name=sip_2_train_89-92
+#SBATCH --nodes=1
+#SBATCH --ntasks=8
+#SBATCH --mem-per-cpu=2gb
+#SBATCH -t 8:00:00
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-user=nigo0024@umn.edu
+#SBATCH -o ./reports/R-%j-%x.out
+#SBATCH -e ./reports/R-%j-%x.err
+
+((start_n=89))
+((n=4))
+((msi_run_id=2))
+start=$(($start_n-1))
+
+# cd ../../.. $SLURM_SUBMIT_DIR
+cd /panfs/roc/groups/5/yangc1/public/hs_process
+CID="5d573462-2134-4900-970d-6e7a5e0f2b1e"
+TT="AgBXVNMKXoOKa6XBlympD0pVKq3EkXxl03NkGB56YPqweBayGeFyClPv0n86GkyPK7PP0mgNM4GqNCk32vwoclEEN"
+TRT="AgxdYn0G06XqDYKmy21kpjmr7xkoP3kdBdrVzPOYag6z3vXp6eCeUNX4Pb4qwa4x9oW5O8DPo3KN684yoNKp05pKrGmyQ"
+export msi_run_id start n CID TT TRT
+
+conda activate sip_run_2
+
+python sip/tune_train.py --n_jobs $SLURM_NTASKS --msi_run_id $msi_run_id --idx_min $start+0 --idx_max $start+$n --CLIENT_ID $CID --TRANSFER_TOKEN $TT --TRANSFER_REFRESH_TOKEN $TRT --feat reflectance --y_label biomass_kgha
+python sip/tune_train.py --n_jobs $SLURM_NTASKS --msi_run_id $msi_run_id --idx_min $start+0 --idx_max $start+$n --CLIENT_ID $CID --TRANSFER_TOKEN $TT --TRANSFER_REFRESH_TOKEN $TRT --feat reflectance --y_label nup_kgha
+python sip/tune_train.py --n_jobs $SLURM_NTASKS --msi_run_id $msi_run_id --idx_min $start+0 --idx_max $start+$n --CLIENT_ID $CID --TRANSFER_TOKEN $TT --TRANSFER_REFRESH_TOKEN $TRT --feat reflectance --y_label tissue_n_pct
+python sip/tune_train.py --n_jobs $SLURM_NTASKS --msi_run_id $msi_run_id --idx_min $start+0 --idx_max $start+$n --CLIENT_ID $CID --TRANSFER_TOKEN $TT --TRANSFER_REFRESH_TOKEN $TRT --feat derivative_1 --y_label biomass_kgha
+python sip/tune_train.py --n_jobs $SLURM_NTASKS --msi_run_id $msi_run_id --idx_min $start+0 --idx_max $start+$n --CLIENT_ID $CID --TRANSFER_TOKEN $TT --TRANSFER_REFRESH_TOKEN $TRT --feat derivative_1 --y_label nup_kgha
+python sip/tune_train.py --n_jobs $SLURM_NTASKS --msi_run_id $msi_run_id --idx_min $start+0 --idx_max $start+$n --CLIENT_ID $CID --TRANSFER_TOKEN $TT --TRANSFER_REFRESH_TOKEN $TRT --feat derivative_1 --y_label tissue_n_pct
+python sip/tune_train.py --n_jobs $SLURM_NTASKS --msi_run_id $msi_run_id --idx_min $start+0 --idx_max $start+$n --CLIENT_ID $CID --TRANSFER_TOKEN $TT --TRANSFER_REFRESH_TOKEN $TRT --feat derivative_2 --y_label biomass_kgha
+python sip/tune_train.py --n_jobs $SLURM_NTASKS --msi_run_id $msi_run_id --idx_min $start+0 --idx_max $start+$n --CLIENT_ID $CID --TRANSFER_TOKEN $TT --TRANSFER_REFRESH_TOKEN $TRT --feat derivative_2 --y_label nup_kgha
+python sip/tune_train.py --n_jobs $SLURM_NTASKS --msi_run_id $msi_run_id --idx_min $start+0 --idx_max $start+$n --CLIENT_ID $CID --TRANSFER_TOKEN $TT --TRANSFER_REFRESH_TOKEN $TRT --feat derivative_2 --y_label tissue_n_pct
+wait
+
+for ((i=0; i < $n; i++))
+do python sip/transfer_data_level.py --CLIENT_ID $CID --TRANSFER_TOKEN $TT --TRANSFER_REFRESH_TOKEN $TRT --msi_run_id $msi_run_id --idx_grid $start+$i --level "segment"
+done
